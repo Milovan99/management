c1e934df3c096a056e535839c02d9ab6d92f4270
Milovan99 <milovanjakovljevic78@gmail.com>
2023-03-16 16:57:58 +0100

    Task Repository

---
+++ src/main/java/com/milovanjakovljevic/management/repositories/TaskRepository.java
@@ -0,0 +1,11 @@
+package com.milovanjakovljevic.management.repositories;
+
+import com.milovanjakovljevic.management.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByProjectIdOrderByDeadlineAsc(Long projectId);
+}
