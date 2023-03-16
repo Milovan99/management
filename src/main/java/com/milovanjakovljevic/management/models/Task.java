e88b83a6b3948ee02c430cc1ef76043b0a148802
Milovan99 <milovanjakovljevic78@gmail.com>
2023-03-16 16:46:46 +0100

    Task Entity

---
+++ src/main/java/com/milovanjakovljevic/management/models/Task.java
@@ -0,0 +1,83 @@
+package com.milovanjakovljevic.management.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String taskSpecification;
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+
+    private LocalDate deadline;
+
+    @ManyToOne
+    private Project project;
+
+    @OneToMany(mappedBy = "parentTask",cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<Subtask> subtasks=new ArrayList<>();
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTaskSpecification() {
+        return taskSpecification;
+    }
+
+    public void setTaskSpecification(String taskSpecification) {
+        this.taskSpecification = taskSpecification;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public List<Subtask> getSubtasks() {
+        return subtasks;
+    }
+
+    public void setSubtasks(List<Subtask> subtasks) {
+        this.subtasks = subtasks;
+    }
+}
