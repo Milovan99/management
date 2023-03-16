3284c3d29ea9cc077c4c95b9a7fb803846787e58
Milovan99 <milovanjakovljevic78@gmail.com>
2023-03-16 17:25:22 +0100

    Task Service

---
+++ src/main/java/com/milovanjakovljevic/management/services/TaskService.java
@@ -0,0 +1,32 @@
+package com.milovanjakovljevic.management.services;
+
+import com.milovanjakovljevic.management.models.Task;
+import com.milovanjakovljevic.management.repositories.TaskRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public static Task getTaskById(Long taskId) {
+        return taskRepository.findById(taskId)
+                .orElseThrow(() -> new EntityNotFoundException("Task not found"));
+    }
+
+    public static void saveTask(Task task) {
+        taskRepository.save(task);
+    }
+
+    public void deleteTask(Long taskId) {
+        taskRepository.deleteById(taskId);
+    }
+}
