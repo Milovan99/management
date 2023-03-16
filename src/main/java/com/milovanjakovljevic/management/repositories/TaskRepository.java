7fb95a2923c9cae38067d319bfe31a441aef9593
Milovan99 <milovanjakovljevic78@gmail.com>
2023-03-16 17:42:18 +0100

    Count By Project Id And Status

--- src/main/java/com/milovanjakovljevic/management/repositories/TaskRepository.java
+++ src/main/java/com/milovanjakovljevic/management/repositories/TaskRepository.java
@@ -1,6 +1,7 @@
 package com.milovanjakovljevic.management.repositories;
 
 import com.milovanjakovljevic.management.models.Task;
+import com.milovanjakovljevic.management.models.TaskStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
@@ -8,4 +9,5 @@ import java.util.List;
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findByProjectId(Long projectId);
+    long countByProjectIdAndStatus(Long projectId, TaskStatus status);
 }
